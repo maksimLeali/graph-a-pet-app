9928ee0c332a54e4ee427caaf0221e30fa4289b5
DESKTOP-EOIIK29\Maks <m.leali@duckma.com>
2023-05-26 15:35:19 +0200

    feat: better form handling

--- src/modules/auth/pages/Login.tsx
+++ src/modules/auth/pages/Login.tsx
@@ -59,7 +59,7 @@ export const Login: React.FC = ()  => {
                         registerOptions={{pattern:  /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/}}
                         ntTextLabel="Email"
                     />
-                    <TextInput type='password' name="password" ntTextLabel="Password" />
+                    <TextInput required type='password' name="password" ntTextLabel="Password" />
                     <SubmitInput color="primary">{t('auth.login')} </SubmitInput>
 
                 </Form>
