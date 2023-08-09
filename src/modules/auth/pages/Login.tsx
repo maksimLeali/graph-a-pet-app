678ff11a178743985a664c181cda223d5ae59628
Maksim <maksim@gothamsiti.it>
2023-08-09 11:15:57 +0200

    feat: wip date-time picker

--- src/modules/auth/pages/Login.tsx
+++ src/modules/auth/pages/Login.tsx
@@ -4,7 +4,7 @@ import { FormProvider, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
-import { Icon, SubmitInput, TextInput, TextAreaInput } from "../../../components";
+import { Icon, SubmitInput, TextInput, TextAreaInput, DateTimePicker } from "../../../components";
 import { MutationLoginArgs } from "../../../types";
 import { useLoginMutation } from "../operations/__generated__/login.generated";
 import { useHistory } from "react-router";
@@ -64,8 +64,8 @@ export const Login: React.FC = ()  => {
                         textLabel="auth.email"
                     />
                     <TextInput required type='password' name="password" textLabel="auth.password" />
+                    <DateTimePicker name="test" ntTextLabel="test"  />
                     <SubmitInput color="primary">{t('auth.login')} </SubmitInput>
-
                 </Form>
             </FormProvider>
             <InfoBox>
