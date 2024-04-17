fd32058c4bb3e881b9f74f28afda9877ad7ec821
maksim-DUFERCO <maksim.leali@dufercodev.com>
2024-04-17 14:55:08 +0200

    fix: 0x0 on images

--- src/components/Image2x.tsx
+++ src/components/Image2x.tsx
@@ -1,44 +1,71 @@
-import { useEffect, useRef, useState } from "react"
-import styled from "styled-components"
-import { config } from "../config"
-import { useAppContext } from "../contexts"
-
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import { config } from "../config";
+import { useAppContext } from "../contexts";
 
 type props = {
-    id: string,
-    alt?: string, 
-    fit?: boolean,
-    rounded?: boolean
-    className?: string,
-    lazy?: boolean
-}
+	id: string;
+	alt?: string;
+	fit?: boolean;
+	rounded?: boolean;
+	className?: string;
+	lazy?: boolean;
+};
 
-export const Image2x: React.FC<props>= ({id, alt, fit= false, rounded=false ,className, lazy})=> {
-    const [src,setSrc] = useState<string>() ;
-    const [src2x, setSrc2x] =useState<string>() ; 
-    const ref = useRef<HTMLDivElement>(null)
-    const { webpSupported } = useAppContext()
-    
-    useEffect(()=> {
-        const baseUrl = `${config.baseUrl?.replace('graphql', 'media')}/${id}`
-        const parameters = [
-            ...(webpSupported ? ["format=webp"] : [])
-        ]
+export const Image2x: React.FC<props> = ({
+	id,
+	alt,
+	fit = false,
+	rounded = false,
+	className,
+	lazy,
+}) => {
+	const [src, setSrc] = useState<string>();
+	const [src2x, setSrc2x] = useState<string>();
+	const ref = useRef<HTMLDivElement>(null);
+	const { webpSupported } = useAppContext();
 
-        setSrc (`${baseUrl}/${ref.current ? ref.current.offsetWidth : 0}x${ref.current ? ref.current.offsetHeight : 0}${fit ? '/fit' : ''}${parameters.length > 0 ? '?' + parameters.join('&') : ''}`)
-        setSrc2x (`${baseUrl}/${ref.current ? ref.current.offsetWidth*2 : 0}x${ref.current ? ref.current.offsetHeight*2 : 0}${fit ? '/fit' : ''}${parameters.length > 0 ? '?' + parameters.join('&') : ''}`)
-    }, [ref.current?.offsetHeight, ref.current?.offsetWidth])
-    return <ImageContainer className={`img2x ${className}`} ref={ref} rounded>
-        {src && src2x &&<img src={src} alt={alt ?? `${id}`} srcSet={`${src2x} 2x`} loading={ lazy ? "lazy" : 'eager'} />}
-    </ImageContainer>
-}
+	useEffect(() => {
+		const baseUrl = `${config.baseUrl?.replace("graphql", "media")}/${id}`;
+		const parameters = [...(webpSupported ? ["format=webp"] : [])];
+		const width = ref.current?.offsetWidth ?? 0;
+		const height = ref.current?.offsetHeight ?? 0;
 
-const ImageContainer = styled.div<{rounded: boolean}>`
-    ${({rounded})=> rounded && `border-radius: 999px; overflow: hidden;`}
-    width:100%;
-    height: 100%;
-    > img {
-        width: 100%;
-        height:  100%;
-    }
-`
+        if(width==0 && height == 0) return;
+		const srcTemp = `${baseUrl}/${width}x${height}${fit ? "/fit" : ""}${
+			parameters.length > 0 ? "?" + parameters.join("&") : ""
+		}`;
+		console.log(srcTemp);
+		setSrc(srcTemp);
+		setSrc2x(
+			`${baseUrl}/${width * 2}x${height}${fit ? "/fit" : ""}${
+				parameters.length > 0 ? "?" + parameters.join("&") : ""
+			}`
+		);
+	}, [ref.current?.offsetHeight, ref.current?.offsetWidth]);
+	return (
+		<ImageContainer className={`img2x ${className}`} ref={ref} rounded>
+			{src &&
+				ref.current &&
+				(ref.current.offsetHeight > 0 || ref.current.offsetWidth > 0) &&
+				src2x && (
+					<img
+						src={src}
+						alt={alt ?? `${id}`}
+						srcSet={`${src2x} 2x`}
+						loading={lazy ? "lazy" : "eager"}
+					/>
+				)}
+		</ImageContainer>
+	);
+};
+
+const ImageContainer = styled.div<{ rounded: boolean }>`
+	${({ rounded }) => rounded && `border-radius: 999px; overflow: hidden;`}
+	width:100%;
+	height: 100%;
+	> img {
+		width: 100%;
+		height: 100%;
+	}
+`;
